87b37e43c6149805cd4e8d95676c64f3ff31700c
afroCoderHanane <gbadhanane@gmail.com>
2022-03-10 16:38:13 -0800

    update

--- run_viz.sh
+++ run_viz.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for hashtag in '#코로나바이러스' '#コロナウイルス' '#冠状病毒' '#covid2019' '#covid-2019' '#covid19' '#covid-19' '#coronavirus' '#corona' '#virus' '#flu' '#sick' '#cough' '#sneeze' '#hospital' '#nurse' '#doctor'; do  ./src/visualize.py --input_path=reduced.lang --key=$hashtag | head > viz/$hashtag; done ;
+for hashtag in '#코로나바이러스' '#コロナウイルス' '#冠状病毒' '#covid2019' '#covid-2019' '#covid19' '#covid-19' '#coronavirus' '#corona' '#virus' '#flu' '#sick' '#cough' '#sneeze' '#hospital' '#nurse' '#doctor'; do  ./src/visualize.py --input_path=reduced.country --key=$hashtag | head > viz/country/$hashtag; done ;
